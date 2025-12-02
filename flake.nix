e16e07387ae4a13e435012ccb6583e5b0fbe281a
alper-han <89567766+alper-han@users.noreply.github.com>
2025-12-02 18:02:39 +0300

    Bump nix flake version to 0.3.0

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
         # The main CrossMacro package
         crossmacro = pkgs.buildDotnetModule rec {
           pname = "crossmacro";
-          version = "0.2.0";
+          version = "0.3.0";
 
           src = ./.;
 
